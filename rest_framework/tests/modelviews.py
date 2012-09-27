2b9526761e5199ff1f543d74b0028a5cf2d4ed3e
Charpentier Johan <jcharpentier@bearstech.com>
2012-09-27 16:00:02 +0200

    Add an example with model
    Here I use a simple model with views/serializer as we can see in the
    tutorial and try to get PUT and POST form.

--- rest_framework/tests/modelviews.py
+++ rest_framework/tests/modelviews.py
@@ -1,53 +1,82 @@
-# from django.conf.urls.defaults import patterns, url
-# from django.forms import ModelForm
-# from django.contrib.auth.models import Group, User
+from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.models import Group, User
 # from rest_framework.resources import ModelResource
 # from rest_framework.views import ListOrCreateModelView, InstanceModelView
 # from rest_framework.tests.models import CustomUser
-# from rest_framework.tests.testcases import TestModelsTestCase
+from rest_framework.tests.testcases import TestModelsTestCase
+from rest_framework import serializers
+from rest_framework import generics
 
 
-# class GroupResource(ModelResource):
-#     model = Group
+class GroupSerializer(serializers.ModelSerializer):
+    model = Group
 
 
-# class UserForm(ModelForm):
-#     class Meta:
-#         model = User
-#         exclude = ('last_login', 'date_joined')
+class GroupDetailView(generics.InstanceAPIView):
+    model = Group
+    serializer_class = GroupSerializer
 
 
-# class UserResource(ModelResource):
+class GroupView(generics.RootAPIView):
+    model = Group
+    serializer_class = GroupSerializer
+
+
+# class UserSerializer(serializers.ModelSerializer):
 #     model = User
-#     form = UserForm
 
+# class UserDetailView(generics.InstanceAPIView):
+#     model = User
+#     serializer_class = UserSerializer
 
-# class CustomUserResource(ModelResource):
-#     model = CustomUser
+urlpatterns = patterns('',
+    # url(r'^users/$', UserView.as_view(), name="users"),
+    # url(r'^users/(?P<pk>[^/]+)/$', UserDetailView.as_view(), name="users-detail"),
+    url(r'^groups/$', GroupView.as_view(), name="groups"),
+    url(r'^groups/(?P<pk>[^/]+)/$', GroupDetailView.as_view(), name="groups-detail"),
 
-# urlpatterns = patterns('',
-#     url(r'^users/$', ListOrCreateModelView.as_view(resource=UserResource), name='users'),
-#     url(r'^users/(?P<id>[0-9]+)/$', InstanceModelView.as_view(resource=UserResource)),
 #     url(r'^customusers/$', ListOrCreateModelView.as_view(resource=CustomUserResource), name='customusers'),
 #     url(r'^customusers/(?P<id>[0-9]+)/$', InstanceModelView.as_view(resource=CustomUserResource)),
-#     url(r'^groups/$', ListOrCreateModelView.as_view(resource=GroupResource), name='groups'),
-#     url(r'^groups/(?P<id>[0-9]+)/$', InstanceModelView.as_view(resource=GroupResource)),
-# )
+)
 
 
-# class ModelViewTests(TestModelsTestCase):
-#     """Test the model views rest_framework provides"""
-#     urls = 'rest_framework.tests.modelviews'
+class ModelViewTests(TestModelsTestCase):
+    """Test the model views rest_framework provides"""
+    urls = 'rest_framework.tests.modelviews'
 
-#     def test_creation(self):
-#         """Ensure that a model object can be created"""
-#         self.assertEqual(0, Group.objects.count())
+    def test_detail(self):
+        """Ensure that a model can be retrieved and has a PUT form"""
+        self.assertEqual(0, Group.objects.count())
 
-#         response = self.client.post('/groups/', {'name': 'foo'})
+        gp1 = Group.objects.create(name='foo')
+        resp = self.client.get('/groups/%s/' % gp1.pk)
 
-#         self.assertEqual(response.status_code, 201)
-#         self.assertEqual(1, Group.objects.count())
-#         self.assertEqual('foo', Group.objects.all()[0].name)
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "foo")
+
+        # Test for HTML PUT form
+        resp = self.client.get('/groups/%s/' % gp1.pk,
+                                HTTP_ACCEPT="text/html")
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('name' in resp.context["put_form"].fields)
+
+    def test_creation(self):
+        """Ensure that a model object can be created"""
+        self.assertEqual(0, Group.objects.count())
+
+        # XXX : Won't work
+        # resp = self.client.post('/groups/', {'name': 'foo'})
+
+        # self.assertEqual(resp.status_code, 201)
+        # self.assertEqual(1, Group.objects.count())
+        # self.assertEqual('foo', Group.objects.all()[0].name)
+
+        # Test for HTML PUT form
+        resp = self.client.get('/groups/', HTTP_ACCEPT="text/html")
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('name' in resp.context["post_form"].fields)
 
 #     def test_creation_with_m2m_relation(self):
 #         """Ensure that a model object with a m2m relation can be created"""
